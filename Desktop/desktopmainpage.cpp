b5f83bf26e3662a15345a0624bb82a81c8e6d5c5
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-11-03 11:53:49 +0300

    Header Class and Footer Class Created

--- Desktop/desktopmainpage.cpp
+++ Desktop/desktopmainpage.cpp
@@ -4,5 +4,63 @@
 
 MainPage::DesktopMainPage::DesktopMainPage()
 {
-    addWidget(cpp14::make_unique<WText>("Main Page"));
+    addWidget(cpp14::make_unique<Header>());
+
+    addWidget(cpp14::make_unique<Footer>());
+}
+
+MainPage::Header::Header()
+{
+    setStyleClass("introheader");
+
+    setContentAlignment(Wt::AlignmentFlag::Center);
+
+    mLayout = setLayout(cpp14::make_unique<WHBoxLayout>());
+    mLayout->setContentsMargins(0,0,0,0);
+
+
+    mEUFlag = mLayout->addWidget(cpp14::make_unique<WImage>(WLink("/img/EurpanCommisionFlag.jpg")),0,Wt::AlignmentFlag::Right);
+
+    auto mDevLayout = mLayout->addLayout(cpp14::make_unique<WVBoxLayout>());
+    mDevLayout->addStretch(1);
+    auto mTitle = mDevLayout->addWidget(cpp14::make_unique<WText>("Development of Innovation"),0,Wt::AlignmentFlag::Left);
+    mTitle->setStyleClass("introDevelopment_of_Innovation");
+    mDevLayout->addStretch(1);
+
+    auto mflagLayout = mDevLayout->addLayout(cpp14::make_unique<WHBoxLayout>());
+
+    int spaceWidth = 31;
+    auto english = mflagLayout->addWidget(cpp14::make_unique<WImage>(WLink("img/flags/en.jpg")),0,Wt::AlignmentFlag::Left);
+    english->setMaximumSize(46,28);
+    mflagLayout->addSpacing(spaceWidth);
+    auto turkish = mflagLayout->addWidget(cpp14::make_unique<WImage>(WLink("img/flags/tr.jpg")),0,Wt::AlignmentFlag::Left);
+    turkish->setMaximumSize(46,28);
+    mflagLayout->addSpacing(spaceWidth);
+    auto italian = mflagLayout->addWidget(cpp14::make_unique<WImage>(WLink("img/flags/it.jpg")),0,Wt::AlignmentFlag::Left);
+    italian->setMaximumSize(46,28);
+    mflagLayout->addSpacing(spaceWidth);
+    auto german = mflagLayout->addWidget(cpp14::make_unique<WImage>(WLink("img/flags/de.jpg")),0,Wt::AlignmentFlag::Left);
+    german->setMaximumSize(46,28);
+    mflagLayout->addStretch(1);
+
+    english->decorationStyle().setCursor(Wt::Cursor::PointingHand);
+    turkish->decorationStyle().setCursor(Wt::Cursor::PointingHand);
+    italian->decorationStyle().setCursor(Wt::Cursor::PointingHand);
+    german->decorationStyle().setCursor(Wt::Cursor::PointingHand);
+
+    mLayout->addSpacing(150);
+
+
+}
+
+MainPage::Footer::Footer()
+{
+    setStyleClass("introfooter");
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+    auto mogatTitleText = mLayout->addWidget(cpp14::make_unique<WText>("Mogat"),0,Wt::AlignmentFlag::Center);
+    mogatTitleText->setStyleClass("introMogatSubTitle");
+    auto text = mLayout->addWidget(cpp14::make_unique<WText>(legalNoitice),0,Wt::AlignmentFlag::Center|Wt::AlignmentFlag::Bottom);
+    text->setStyleClass("introlegalNotice");
+    auto text2 = mLayout->addWidget(cpp14::make_unique<WText>("© 2017 MOGAT • powered by Wt & Qt & github"),0,Wt::AlignmentFlag::Center);
+    text2->setStyleClass("introcopyRight");
 }
--- Desktop/desktopmainpage.h
+++ Desktop/desktopmainpage.h
@@ -18,10 +18,50 @@ using namespace Wt;
 namespace MainPage {
 
 
+    class Header;
+
+    class Footer;
+
+
+    ///
+    /// \brief The DesktopMainPage class
+    /// Main Page Container Class.
     class DesktopMainPage : public WContainerWidget
     {
     public:
         DesktopMainPage();
+    };
+
+
+    ///
+    /// \brief The Header class
+    /// Main Page Header Container Class
+    class Header : public WContainerWidget
+    {
+    public:
+        Header();
+
+        WImage* mEUFlag;
+
+        WHBoxLayout* mLayout;
+    };
+
+
+
+
+    ///
+    /// \brief Main page's Footer Design Code
+    /// This Class include Legal information about MOGAT, its own documentary,
+    /// application or any others
+    class Footer : public WContainerWidget
+    {
+    public:
+        Footer();
+
+    private:
+        const std::string legalNoitice{"This project has been financed with the support of the European Commission."
+                                        "The authors are solely responsible for this publication and the Commission does not accept responsibility for the use that could be made of the information contained within it."
+                                        "All the content on this website, including but not limited to, the name, the logo, the web pages content (text and images) and any downloadable file, belong to MOGAT. The use of this content is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License."};
     };
 }
 
