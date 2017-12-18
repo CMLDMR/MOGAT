3ff2710aa73b79b15cd27fbe04e5752a80351537
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-12-18 19:31:08 +0300

    New user Login Widget Added

---
+++ Desktop/userloginwidget.cpp
@@ -0,0 +1,6 @@
+#include "userloginwidget.h"
+
+UserLoginWidget::UserLoginWidget()
+{
+
+}
---
+++ Desktop/userloginwidget.h
@@ -0,0 +1,12 @@
+#ifndef USERLOGINWIDGET_H
+#define USERLOGINWIDGET_H
+
+#include <QtCore/qglobal.h>
+
+class UserLoginWidget : public WContainerWidget
+{
+public:
+    UserLoginWidget();
+};
+
+#endif // USERLOGINWIDGET_H
